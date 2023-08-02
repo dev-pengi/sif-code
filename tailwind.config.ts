c13ff29319817d5986949759286d4d26c6867f1f
dev-pengi <siffffdoul@gmail.com>
2023-08-02 14:58:16 +0100

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      screens: {
+        vsm: "370px",
+        xmd: "920px",
+        xlg: "1300px",
+      },
+      colors: {
+      },
+      spacing: {
+        navD: "60px",
+      },
+      boxShadow: {
+        headerShadow: "rgba(0, 0, 0, 0.03) 0px 10px 50px;",
+      },
+    },
+  },
+  plugins: [],
+};
