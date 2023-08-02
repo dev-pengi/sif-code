5f40af1d584ccea050da68bb05b36588ae85498c
dev-pengi <siffffdoul@gmail.com>
2023-08-02 19:15:29 +0100

    New assets

---
+++ src/assets/index.ts
@@ -0,0 +1,17 @@
+import cssIcon from "./css.svg";
+import htmlIcon from "./html.svg";
+import javascriptIcon from "./js.svg";
+import downloadIcon from "./download.svg";
+import themesIcon from "./themes.svg";
+import rotateIcon from "./rotate.svg";
+import devIcon from "./dev.svg";
+
+export {
+  cssIcon,
+  htmlIcon,
+  javascriptIcon,
+  downloadIcon,
+  themesIcon,
+  rotateIcon,
+  devIcon,
+};
