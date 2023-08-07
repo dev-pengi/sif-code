bce221ec28ec485a01f788b606a552e794dc062e
dev-pengi <siffffdoul@gmail.com>
2023-08-07 14:35:23 +0100

    made a better performance approach

--- src/components/Ide/IDE.tsx
+++ src/components/Ide/IDE.tsx
@@ -14,27 +14,6 @@ const IDE: FC = () => {
     codeHeight,
     isHorizontal,
   } = useCodeContext();
-  const { isLoaded } = useFilesContext();
-  const [style, setStyle] = useState<CSSProperties>({});
-
-  useEffect(() => {
-    const style = {
-      height: `calc(100vh - 60px)`,
-      width: `100vw`,
-      marginTop: "60px",
-      marginLeft: "0",
-    };
-
-    if (smallScreen) {
-      style.width = `calc(100vw - 60px)`;
-      style.height = "100vh";
-      style.marginTop = "0";
-      style.marginLeft = "60px";
-    }
-
-    setStyle(style);
-  }, [smallScreen]);
-
   return (
     <main
       className={`py-[5px] px-[5px] vsm:py-0 vsm:pl-[5px] pl-0 w-[calc(100vw-60px)] h-screen mt-0 ml-[60px] vsm:w-full vsm:h-[calc(100vh-60px)] vsm:mt-[60px] vsm:ml-0`}
