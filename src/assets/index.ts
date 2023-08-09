0a9e902efe9cb77975cafebcc75c48b7efed6bfd
dev-pengi <siffffdoul@gmail.com>
2023-08-09 21:03:48 +0100

    Updated asstes

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,11 @@
 import cssIcon from "./css.svg";
 import htmlIcon from "./html.svg";
-import javascriptIcon from "./js.svg";
+import javascriptIcon from "./javascript.svg";
 import downloadIcon from "./download.svg";
 import themesIcon from "./themes.svg";
 import rotateIcon from "./rotate.svg";
 import devIcon from "./dev.svg";
+import plusIcon from "./plus.svg";
 
 export {
   cssIcon,
@@ -14,4 +15,5 @@ export {
   themesIcon,
   rotateIcon,
   devIcon,
+  plusIcon,
 };
