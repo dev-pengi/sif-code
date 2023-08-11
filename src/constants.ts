703dbb29038c7f78154d793909c8a588df2319f7
dev-pengi <siffffdoul@gmail.com>
2023-08-11 16:34:20 +0100

    Add modals styles to the constants file

--- src/constants.ts
+++ src/constants.ts
@@ -28,5 +28,49 @@ const initialFiles: File[] = [
   },
 ];
 
-export { initialFiles, initialCodes };
+const ModalStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    width: "90%",
+    maxWidth: "650px",
+    minHeight: "200px",
+    height: "max-content",
+    maxHeight: "700px",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    padding: "15px 15px",
+    background: "#343436",
+    border: "none",
+  },
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.4)",
+    zIndex: 1000,
+  },
+};
+const ConfirmationModalStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    width: "max-content",
+    minWidth: "450px",
+    maxWidth: "500px",
+    minHeight: "100px",
+    height: "max-content",
+    maxHeight: "700px",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    padding: "15px 15px",
+    background: "#343436",
+    border: "none",
+  },
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.4)",
+    zIndex: 1000,
+  },
+};
+
+export { initialFiles, initialCodes, ModalStyles, ConfirmationModalStyles };
 export type { File, FileType };
